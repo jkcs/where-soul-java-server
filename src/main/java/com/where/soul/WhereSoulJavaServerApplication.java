6f7f2a56e54d253e07e5dfef261886bb89da24f1
liuwei <liuwei@woaizuji.com>
2019-11-27 17:41:59 +0800

    :tada: 项目启动

--- src/main/java/com/where/soul/WhereSoulJavaServerApplication.java
+++ src/main/java/com/where/soul/WhereSoulJavaServerApplication.java
@@ -1,4 +1,4 @@
-package com.where.soul.com;
+package com.where.soul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/where/soul/server/WhereSoulJavaServerApplicationTests.java
+++ src/test/java/com/where/soul/server/WhereSoulJavaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.where.soul.com.server;
+package com.where.soul.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
