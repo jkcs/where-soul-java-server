ae579f133ee086865edff96810bd0be005a3b7fe
liuwei <liuwei@woaizuji.com>
2019-11-27 16:54:25 +0800

    :tada: init commit

---
+++ src/main/java/com/where/soul/com/WhereSoulJavaServerApplication.java
@@ -0,0 +1,16 @@
+package com.where.soul.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lw
+ */
+@SpringBootApplication
+public class WhereSoulJavaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhereSoulJavaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/where/soul/com/server/WhereSoulJavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.where.soul.com.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereSoulJavaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
